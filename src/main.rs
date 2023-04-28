0c0d24db4e144bf77e1dfc14809e27c813951a18
ntalbs <ntalbsen@gmail.com>
2023-04-28 07:02:02 +0100

    Read/send file for requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{Write, BufReader, BufRead};
+use std::io::{self, ErrorKind, Read, Write, BufReader, BufRead};
 use std::net::{TcpListener, TcpStream};
 use std::str::FromStr;
 use std::thread;
@@ -32,7 +32,10 @@ impl Into<Request> for String {
     fn into(self) -> Request {
         let v = self.split_whitespace().take(2).collect::<Vec<&str>>();
         if let [method, path] = &v[..] {
-            Request { method: method.to_string(), path: trim_path(path).to_string() }
+            Request {
+                method: method.to_string(),
+                path: format!("./{}", trim_path(path).to_string())
+            }
         } else {
             panic!("Fail to get request method/path")
         }
@@ -48,18 +51,45 @@ fn request_line(mut stream: &TcpStream) -> String {
         .unwrap()
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<(), &'static str> {
+const BUF_SIZE: usize = 8 * 1024;
+fn send<R: ?Sized, W: ?Sized>(reader: &mut R, writer: &mut W) -> io::Result<u64> where R: Read, W: Write {
+    let mut buf = [0; BUF_SIZE];
+    let mut written = 0;
+    loop {
+        let len = match reader.read(&mut buf) {
+            Ok(0) => return Ok(written),
+            Ok(len) => len,
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        };
+        writer.write_all(&buf[..len])?;
+        written += len as u64;
+    }
+}
+
+fn http_404(stream: &mut TcpStream) -> io::Result<u64> {
+    let body = b"Not Found\n";
+    stream.write(b"HTTP/1.1 404 Not Fount\n").unwrap();
+    stream.write(b"Content-Type: text/plain\n").unwrap();
+    stream.write(format!("Content-Length: {}\r\n\r\n", body.len()).as_bytes()).unwrap();
+    stream.write(body).unwrap();
+    Err(io::Error::new(ErrorKind::Other, "404 Not Fount"))
+}
+
+fn handle_connection(mut stream: TcpStream) -> io::Result<u64> {
     let request: Request = request_line(&stream).into();
     println!("{} {}", request.method, request.path);
 
-    let body = "Hello, world!\n";
-
-    stream.write(b"HTTP/1.1 200 OK\n").unwrap();
-    stream.write(b"Content-Length: 14\n").unwrap();
-    stream.write(b"Content-Type: text/plain\n\n").unwrap();
-    stream.write(body.as_bytes()).unwrap();
-
-    Ok(())
+    let file = std::fs::File::open(&request.path);
+    return match file {
+        Ok(mut f) => {
+            stream.write(b"HTTP/1.1 200 OK\n").unwrap();
+            stream.write(b"Content-Type: text/plain\n").unwrap();
+            stream.write(format!("Content-Length: {}\r\n\r\n", f.metadata().unwrap().len()).as_bytes()).unwrap();
+            send(&mut f, &mut stream)
+        }
+        Err(_) => http_404(&mut stream),
+    }
 }
 
 fn main() {
