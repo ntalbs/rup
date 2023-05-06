93afc5dd66dc6901d63762c7b7735ef6b41d8faa
ntalbs <ntalbsen@gmail.com>
2023-05-06 08:59:36 +0100

    Improve error response message

--- src/main.rs
+++ src/main.rs
@@ -138,7 +138,8 @@ fn show_dir(stream: &mut TcpStream, path: &Path) -> io::Result<u64> {
 }
 
 fn http_400(stream: &mut TcpStream, reason: & str) -> io::Result<u64> {
-    let body = b"Bad Request\n";
+    let body_string = format!("Bad Request: {}\n", reason);
+    let body = body_string.as_bytes();
     stream.write_all(b"HTTP/1.1 400 Bad Request\n")?;
     stream.write_all(b"Content-Type: text/plain\n")?;
     stream.write_all(format!("Content-Length: {}\r\n\r\n", body.len()).as_bytes())?;
@@ -147,7 +148,8 @@ fn http_400(stream: &mut TcpStream, reason: & str) -> io::Result<u64> {
 }
 
 fn http_404(stream: &mut TcpStream, reason: & str) -> io::Result<u64> {
-    let body = b"Not Found\n";
+    let body_string = format!("Not Found: {}\n", reason);
+    let body = body_string.as_bytes();
     stream.write_all(b"HTTP/1.1 404 Not Fount\n")?;
     stream.write_all(b"Content-Type: text/plain\n")?;
     stream.write_all(format!("Content-Length: {}\r\n\r\n", body.len()).as_bytes())?;
