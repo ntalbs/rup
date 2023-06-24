500a09bc62f822e94f91c2afd542b2c72a0a39c6
ntalbs <ntalbsen@gmail.com>
2023-06-24 11:13:51 +0100

    Rewrite arguments parser

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,9 @@
-use std::{collections::HashMap, process::exit};
+use std::process::exit;
 
 use crate::color::{Color, Style};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const DEFAULT_PORT: u16 = 3000;
-
-pub(crate) struct Args {
-    pub port: u16,
-}
 
 fn show_version() {
     println!("rup {VERSION}");
@@ -36,37 +32,80 @@ fn show_help() {
     print_opt("-V, --version", "", "Print version information");
 }
 
+pub(crate) struct Args {
+    pub port: u16,
+}
+
+enum ParseResult {
+    Args(Args),
+    Help,
+    Version,
+}
+
+struct ParseError {
+    reason: String,
+}
+
 impl Args {
+    fn parse_internal(args: &[String]) -> Result<ParseResult, ParseError> {
+        let mut ret = Self { port: DEFAULT_PORT };
+        let mut args_iter = args.iter();
+
+        loop {
+            if let Some(arg) = args_iter.next() {
+                match arg.as_str() {
+                    "-p" | "--port" => {
+                        if let Some(port_str) = args_iter.next() {
+                            ret.port = match port_str.parse() {
+                                Ok(port) => port,
+                                Err(e) => {
+                                    let reason = format!(
+                                        "Invalid value '{}' for '--port <PORT>': {}",
+                                        port_str, e
+                                    );
+                                    return Err(ParseError { reason })
+                                }
+                            }
+                        } else {
+                            let reason = "error: The argument '--port <PORT>' requires a value but none was supplied".to_string();
+                            return Err(ParseError { reason });
+                        }
+                    }
+                    "-V" | "--version" => {
+                        return Ok(ParseResult::Version);
+                    }
+                    "-h" | "--help" => {
+                        return Ok(ParseResult::Help);
+                    }
+                    _ => {
+                        let mesg = format!("error: Found argument '{}' which wasn't expected, or isn't valid in this context", arg);
+                        return Err(ParseError { reason: mesg });
+                    }
+                }
+            } else {
+                return Ok(ParseResult::Args(ret));
+            }
+        }
+    }
+
     pub(crate) fn parse(args: &[String]) -> Self {
-        let map: HashMap<&str, &str> = args[1..]
-            .chunks_exact(2)
-            .map(|c| (c[0].as_str(), c[1].as_str()))
-            .collect();
-
-        if map.contains_key("-V") || map.contains_key("--version") {
-            show_version();
-            exit(0);
-        }
-
-        if map.contains_key("-h") || map.contains_key("--help") {
-            show_help();
-            exit(0);
-        }
-
-        let port: u16 = map
-            .get("--port")
-            .or_else(|| map.get("-p"))
-            .unwrap_or(&"80")
-            .to_string()
-            .parse()
-            .unwrap_or_else(|e| {
-                eprint!("{}: ", "error".red());
-                eprint!("Invalid value for '");
-                eprint!("{}': ", "--port <PORT>".yellow());
-                eprintln!("{}", e);
+        match Self::parse_internal(&args[1..]) {
+            Ok(r) => match r {
+                ParseResult::Args(a) => return a,
+                ParseResult::Help => {
+                    show_help();
+                    exit(0);
+                }
+                ParseResult::Version => {
+                    show_version();
+                    exit(0);
+                }
+            },
+            Err(e) => {
+                eprintln!("{}", e.reason);
                 exit(1);
-            });
-        Self { port }
+            }
+        }
     }
 }
 
